370f0f70e13d1567ece9249c470304c1b97c9979
Tommy Su <tommy.00912@gmail.com>
2025-06-06 00:21:46 -0700

    fixed issues

--- src/timer_tb.sv
+++ src/timer_tb.sv
@@ -1,24 +1,24 @@
-module tb_timer();
-	reg clk, rst;
-	wire done;
+module timer_tb();
+    reg clk, rst;
+    wire done;
 
-timer #(WAIT_TIME = 3) DUT(.*);
+timer #(3) DUT(.*);
 
 initial begin
-	forever begin
-		clk = 1'b0; #5;
-		clk = 1'b1; #5;
-	end
+    forever begin
+        clk = 1'b0; #5;
+        clk = 1'b1; #5;
+    end
 end
 
 initial begin
-	rst = 1'b1; #6;
-	rst = 1'b0;
+    rst = 1'b1; #6;
+    rst = 1'b0;
 end
 
 initial begin
-	#41;
-	if (DUT.done !== 1'b1)
-		$display("ERROR: TIMER ISNT DONE");
+    #41;
+    if (DUT.done !== 1'b1)
+    $display("ERROR: TIMER ISNT DONE");
 end
 endmodule
